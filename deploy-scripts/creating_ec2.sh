53fa1ee1067c79d965682d6cff5cecbd56a3ece8
eddie <edmundedwingaisie.com>
2024-08-18 07:37:11 +0000

    i have modified the script so that the names of the instances are passed as arguments when you run the script

--- deploy-scripts/creating_ec2.sh
+++ deploy-scripts/creating_ec2.sh
@@ -5,9 +5,14 @@ Instance_type="t2.micro"
 keys="eddieweekend"
 sec_group="launch-wizard-5"
 
+for i in "$@"
+do
 aws ec2 run-instances \
 	--image-id $IMA \
 	--instance-type $Instance_type \
 	--key-name $keys \
 	--security-groups $sec_group \
-	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
+	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+done
+
+
